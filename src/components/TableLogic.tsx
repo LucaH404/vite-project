578b8d9ca7591bef96eb8427f132836b0c24f00a
LucaH404 <luca.sapienza03@gmail.com>
2023-08-10 14:18:58 +0200

    fixed scrolling mechanic

--- src/components/TableLogic.tsx
+++ src/components/TableLogic.tsx
@@ -1,59 +1,48 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Cardcomms from "./Cardcomms";
 import { Comment } from "../models/commentType";
-// import "./Table.css";
-// import CustomTable from "./CustomTable";
 
 const TableLogic = () => {
-  const [comments, setComments] = useState<Comment[]>([])
-  const [limit, setLimitstate] = useState(10)
-  const [show, setShow] = useState(false)
-  const [isScrolled, setisScrolled] = useState(false)
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [limit, setLimitstate] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-      fetchAPI();
-    }, [limit]); 
+    fetchAPI();
+  }, [limit]);
 
   let skip = 0;
   async function fetchAPI() {
     try {
+      setIsLoading(true);
       const url = `https://dummyjson.com/comments?skip=${skip}&limit=${limit}`;
       const response = await fetch(url);
       const data = await response.json();
-      setComments(data.comments)
+      setComments(data.comments);
+      setIsLoading(false);
     } catch (err) {
       console.log("Request Failed", err);
+      setIsLoading(false);
     }
   }
 
   const handleScroll = () => {
-    if (
-      window.innerHeight + window.scrollY >=
-      document.body.scrollHeight - 500
-    ) {
-      // fetchData();
+    if (!isLoading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 500) {
       setLimitstate(limit + 10);
-      console.log(limit);
-      console.log(window.innerHeight);
-      setisScrolled(true);
     }
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-      setisScrolled(false)
-      console.log(isScrolled, limit)
+      window.removeEventListener("scroll", handleScroll);
     };
-  }, [isScrolled]);
+  }, [isLoading]);
 
   return (
     <div>
-      {<Cardcomms
-       comments={comments}
-       limit={limit}
-      />}
+      {<Cardcomms comments={comments} limit={limit} />}
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 };
